1695cf2b7d69387ff3c9b5c75ca1d61e32210292
Ilya Mordvinov <morilya111@gmail.com>
2025-10-30 16:47:01 +0300

    fix: исправить получение лайков на пост     - Исправлены методы в LikeRepository с подгрузкой связных сущностей

--- src/main/java/ru/vsu/cs/OOP/mordvinovil/task2/social_network/repositories/LikeRepository.java
+++ src/main/java/ru/vsu/cs/OOP/mordvinovil/task2/social_network/repositories/LikeRepository.java
@@ -3,6 +3,8 @@ package ru.vsu.cs.OOP.mordvinovil.task2.social_network.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.vsu.cs.OOP.mordvinovil.task2.social_network.entities.Like;
 
@@ -13,6 +15,13 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByUserIdAndCommentId(Long userId, Long commentId);
     Optional<Like> findByUserIdAndPostId(Long userId, Long commentId);
 
-    Page<Like> findByPostId(Long postId, Pageable pageable);
-    Page<Like> findByCommentId(Long commentId, Pageable pageable);
+        @Query("SELECT l FROM Like l " +
+                "LEFT JOIN FETCH l.user " +
+                "WHERE l.post.id = :postId")
+        Page<Like> findByPostId(@Param("postId") Long postId, Pageable pageable);
+
+        @Query("SELECT l FROM Like l " +
+                "LEFT JOIN FETCH l.user " +
+                "WHERE l.comment.id = :commentId")
+        Page<Like> findByCommentId(@Param("commentId") Long commentId, Pageable pageable);
 }
