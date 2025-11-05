03b9dadf0571c496af94723c640c97b4ae409193
Ilya Mordvinov <morilya111@gmail.com>
2025-11-05 13:40:47 +0300

    реализовтаь логирование для  AuthController

--- src/main/java/ru/vsu/cs/OOP/mordvinovil/task2/social_network/controller/auth/AuthController.java
+++ src/main/java/ru/vsu/cs/OOP/mordvinovil/task2/social_network/controller/auth/AuthController.java
@@ -4,8 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +12,10 @@ import ru.vsu.cs.OOP.mordvinovil.task2.social_network.dto.request.auth.SignInReq
 import ru.vsu.cs.OOP.mordvinovil.task2.social_network.dto.request.auth.SignUpRequest;
 import ru.vsu.cs.OOP.mordvinovil.task2.social_network.dto.response.auth.JwtAuthenticationResponse;
 import ru.vsu.cs.OOP.mordvinovil.task2.social_network.service.user.AuthenticationService;
+import ru.vsu.cs.OOP.mordvinovil.task2.social_network.logging.CentralLogger;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -21,23 +23,59 @@ import ru.vsu.cs.OOP.mordvinovil.task2.social_network.service.user.Authenticatio
 @Tag(name = "Аутентификация")
 public class AuthController {
     private final AuthenticationService authenticationService;
-    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+    private final CentralLogger centralLogger;
 
     @Operation(summary = "Регистрация пользователя")
     @PostMapping("/sign-up")
     public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
-        log.info("Запрос на регистрацию пользователя: {}", request.getEmail());
-        JwtAuthenticationResponse response = authenticationService.signUp(request);
-        log.info("Пользователь {} успешно зарегистрирован", request.getEmail());
-        return response;
+        Map<String, Object> context = new HashMap<>();
+        context.put("email", request.getEmail());
+        context.put("username", request.getUsername());
+        context.put("city", request.getCity());
+
+        centralLogger.logInfo("РЕГИСТРАЦИЯ_ЗАПРОС",
+                "Запрос на регистрацию пользователя", context);
+
+        try {
+            JwtAuthenticationResponse response = authenticationService.signUp(request);
+
+            Map<String, Object> successContext = new HashMap<>(context);
+            successContext.put("tokenLength", response.getToken().length());
+
+            centralLogger.logInfo("РЕГИСТРАЦИЯ_УСПЕХ",
+                    "Пользователь успешно зарегистрирован", successContext);
+
+            return response;
+        } catch (Exception e) {
+            centralLogger.logError("РЕГИСТРАЦИЯ_ОШИБКА",
+                    "Ошибка при регистрации пользователя", context, e);
+            throw e;
+        }
     }
 
     @Operation(summary = "Авторизация пользователя")
     @PostMapping("/sign-in")
     public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
-        log.info("Запрос на авторизацию пользователя: {}", request.getUsername());
-        JwtAuthenticationResponse response = authenticationService.signIn(request);
-        log.info("Пользователь {} успешно авторизован", request.getUsername());
-        return response;
+        Map<String, Object> context = new HashMap<>();
+        context.put("username", request.getUsername());
+
+        centralLogger.logInfo("АВТОРИЗАЦИЯ_ЗАПРОС",
+                "Запрос на авторизацию пользователя", context);
+
+        try {
+            JwtAuthenticationResponse response = authenticationService.signIn(request);
+
+            Map<String, Object> successContext = new HashMap<>(context);
+            successContext.put("tokenLength", response.getToken().length());
+
+            centralLogger.logInfo("АВТОРИЗАЦИЯ_УСПЕХ",
+                    "Пользователь успешно авторизован", successContext);
+
+            return response;
+        } catch (Exception e) {
+            centralLogger.logError("АВТОРИЗАЦИЯ_ОШИБКА",
+                    "Ошибка при авторизации пользователя", context, e);
+            throw e;
+        }
     }
 }
