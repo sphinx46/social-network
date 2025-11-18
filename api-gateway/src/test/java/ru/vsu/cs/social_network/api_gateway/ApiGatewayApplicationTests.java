20941283fe71a47ef8c2a83ebb5187626039fd8b
Ilya Mordvinov <morilya111@gmail.com>
2025-11-18 21:53:53 +0300

    feat: добавить реактивную фабрику для RedisConfig

--- api-gateway/src/test/java/ru/vsu/cs/social_network/api_gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/ru/vsu/cs/social_network/api_gateway/ApiGatewayApplicationTests.java
@@ -2,12 +2,16 @@ package ru.vsu.cs.social_network.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Простой тест для проверки загрузки контекста
+		// В тестовом профиле Gateway и Security конфигурации упрощены
 	}
 
 }
