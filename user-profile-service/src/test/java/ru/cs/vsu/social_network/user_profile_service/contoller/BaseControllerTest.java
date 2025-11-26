ad4feae8b35cac4841eaf5e8cbbb8dd275d5725e
Ilya Mordvinov <morilya111@gmail.com>
2025-11-27 00:47:30 +0300

    fix: удаление неиспользующегося jwt service из user-profile-service

--- user-profile-service/src/test/java/ru/cs/vsu/social_network/user_profile_service/contoller/BaseControllerTest.java
+++ user-profile-service/src/test/java/ru/cs/vsu/social_network/user_profile_service/contoller/BaseControllerTest.java
@@ -4,11 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import ru.cs.vsu.social_network.user_profile_service.service.serviceImpl.JwtServiceImpl;
 import ru.cs.vsu.social_network.user_profile_service.utils.MockMvcUtils;
 
 /**
@@ -36,11 +34,6 @@ public abstract class BaseControllerTest {
     @Autowired
     protected ObjectMapper objectMapper;
 
-    /**
-     * Мок сервиса JWT аутентификации для тестирования защищенных endpoints.
-     */
-    @MockitoBean
-    protected JwtServiceImpl jwtServiceImpl;
 
     /**
      * Контекст Spring приложения для настройки MockMvc.
