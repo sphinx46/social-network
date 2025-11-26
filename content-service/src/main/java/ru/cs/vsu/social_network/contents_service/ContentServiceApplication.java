e368f4671944894af7476318a7e9e16a0c433d9d
Ilya Mordvinov <morilya111@gmail.com>
2025-11-27 00:04:25 +0300

    chore: реализовать базовые .yaml конфигурации для content-service

--- content-service/src/main/java/ru/cs/vsu/social_network/contents_service/ContentServiceApplication.java
+++ content-service/src/main/java/ru/cs/vsu/social_network/contents_service/ContentServiceApplication.java
@@ -2,7 +2,9 @@ package ru.cs.vsu.social_network.contents_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ContentServiceApplication {
 
--- content-service/src/test/java/ru/cs/vsu/social_network/contents_service/ContentServiceApplicationTests.java
+++ content-service/src/test/java/ru/cs/vsu/social_network/contents_service/ContentServiceApplicationTests.java
@@ -2,8 +2,10 @@ package ru.cs.vsu.social_network.contents_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ContentServiceApplicationTests {
 
     @Test
