13e259ab8e520c36491f8737717722216d364b5a
Ilya Mordvinov <morilya111@gmail.com>
2025-12-02 16:14:13 +0300

    feat: реализовать mapping слой для корректного преобразования entity в DTO

---
+++ messaging-service/src/main/java/ru/cs/vsu/social_network/messaging_service/mapping/config/ModelMapperConfig.java
@@ -0,0 +1,67 @@
+package ru.cs.vsu.social_network.messaging_service.mapping.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.cs.vsu.social_network.messaging_service.dto.response.messaging.MessageResponse;
+import ru.cs.vsu.social_network.messaging_service.entity.Message;
+
+/**
+ * Конфигурационный класс для настройки ModelMapper.
+ * Определяет правила маппинга между сущностями, DTO и событиями.
+ * Обеспечивает корректное преобразование данных между слоями приложения.
+ *
+ */
+@Slf4j
+@Configuration
+public class ModelMapperConfig {
+
+    /**
+     * Создает и настраивает ModelMapper для преобразования сущностей в DTO и события.
+     * Включает сопоставление полей, пропуск null значений и доступ к приватным полям.
+     *
+     * @return настроенный экземпляр ModelMapper с определенными правилами маппинга
+     */
+    @Bean
+    public ModelMapper modelMapper() {
+        log.info("MODEL_MAPPER_КОНФИГУРАЦИЯ_НАЧАЛО");
+
+        final ModelMapper modelMapper = new ModelMapper();
+
+        configureMessageMappings(modelMapper);
+
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(
+                        org.modelmapper.config.Configuration
+                                .AccessLevel.PRIVATE);
+
+        log.info("MODEL_MAPPER_КОНФИГУРАЦИЯ_УСПЕХ");
+        return modelMapper;
+    }
+
+    /**
+     * Настраивает маппинг между сущностью Message и MessageResponse.
+     * Определяет правила преобразования полей сообщения в DTO ответа.
+     *
+     * @param modelMapper экземпляр ModelMapper для настройки
+     */
+    private void configureMessageMappings(final ModelMapper modelMapper) {
+        modelMapper.addMappings(new PropertyMap<Message, MessageResponse>() {
+            @Override
+            protected void configure() {
+                map().setConversationId(source.getConversation().getId());
+                map().setReceiverId(source.getReceiverId());
+                map().setSenderId(source.getSenderId());
+                map().setContent(source.getContent());
+                map().setImageUrl(source.getImageUrl());
+                map().setCreatedAt(source.getCreatedAt());
+                map().setUpdatedAt(source.getUpdatedAt());
+            }
+        });
+    }
+
+}
